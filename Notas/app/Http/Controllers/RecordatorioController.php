41768b994b6f31aeb812b80b0ee4612017844854
Jeddiel123 <jeddiel.19070058@itsmotul.edu.mx>
2023-05-12 19:32:58 -0600

    Notas

--- Notas/app/Http/Controllers/RecordatorioController.php
+++ Notas/app/Http/Controllers/RecordatorioController.php
@@ -2,9 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recordatorio;
 use Illuminate\Http\Request;
 
 class RecordatorioController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Recordatorio  $recordatorio
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Recordatorio $recordatorio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Recordatorio  $recordatorio
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Recordatorio $recordatorio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Recordatorio  $recordatorio
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Recordatorio $recordatorio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Recordatorio  $recordatorio
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Recordatorio $recordatorio)
+    {
+        //
+    }
 }
